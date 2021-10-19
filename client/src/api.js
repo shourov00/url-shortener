86dafe68b884153ec6b7cd81be80db4bec8cfd47
shourov <shohidshourov@gmail.com>
2021-10-19 22:20:26 +0600

    Deployed to heroku

--- client/src/api.js
+++ client/src/api.js
@@ -1,10 +1,10 @@
 import url from './api/url'
 
-export const base_url = "http://localhost:5000/api"
-export const website = "http://localhost:3000/"
+// export const base_url = "http://localhost:5000/api"
+// export const website = "http://localhost:3000/"
 
-// export const base_url = "http://https://url-shortener-rabbi.herokuapp.com/api"
-// export const website = "http://https://url-shortener-rabbi.herokuapp.com/"
+export const base_url = "http://https://url-shortener-rabbi.herokuapp.com/api"
+export const website = "http://https://url-shortener-rabbi.herokuapp.com/"
 
 // export const base_url = ""
 
